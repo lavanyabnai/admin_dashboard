0e30e5a15f7eb150163f44b85448f9d4d535e017
lavanyabnai <lavanya.bnai@gmail.com>
2023-01-30 18:58:01 +0530

    add admin-dashboard

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,87 +1,77 @@
-import React from 'react';
-import { Form, Field, FormElement } from '@progress/kendo-react-form';
-import { Input } from '@progress/kendo-react-inputs';
-import { Checkbox } from '@progress/kendo-react-inputs';
-import { Link } from 'react-router-dom';
-import { Button } from '@progress/kendo-react-buttons';
-
+import React from "react";
+import { Form, Field, FormElement } from "@progress/kendo-react-form";
+import { Input } from "@progress/kendo-react-inputs";
+import { Checkbox } from "@progress/kendo-react-inputs";
+import { Link } from "react-router-dom";
 
 export const SignIn = () => {
-  const [paswordType, setPasswordType] = React.useState('password')
+  const [paswordType] = React.useState("password");
+  return (
+    <div className="App">
+      <div className="sign-in-page">
+        <div className="sign-in-wrapper">
+          <div className="logo-wrapper">
+            <div className="logo">
+              <img
+                src={require("../assets/logo/logo-white.png")}
+                alt={"sign in icon"}
+              />
+            </div>
+            <div className="banner">Sign In</div>
+            <div className="account text-white">
+              Don't have an account? <Link to="/signup">Sign up</Link>
+            </div>
+          </div>
+          <div className="inputs-wrapper">
+            <Form
+              render={(formRenderProps) => (
+                <FormElement
+                  style={{
+                    maxWidth: 650,
+                  }}
+                >
+                  <fieldset className={"k-form-fieldset"}>
+                    <div className="mb-3">
+                      <Field
+                        name={"email"}
+                        type={"email"}
+                        component={Input}
+                        label={"Email"}
+                      />
+                    </div>
+                    <div className="mb-3" style={{ display: "flex" }}>
+                      <Field
+                        name={"password"}
+                        type={paswordType}
+                        component={Input}
+                        label={"Password"}
+                      />
+                    </div>
+                  </fieldset>
+                  <fieldset className={"k-form-fieldset"}>
+                    <div className="mb-3 text-white">
+                      <Checkbox label={"Remember Me"} />
+                    </div>
+                  </fieldset>
 
-  const handleClick = () => {
-    if (paswordType === 'password') {
-      setPasswordType('text')
-    } else if (paswordType === 'text') {
-      setPasswordType('password')
-    }
-  }
-   return (
-      <div className="App ">
-        <div className="sign-in-page">
-           <div className="sign-in-wrapper">
-            
-               <div className="logo-wrapper">
-     
-         <div className="logo">
-         <img src={require('../assets/logo/Original on Transparent.png')} alt={'logo'} />
-         </div>
-         <div className='banner'>
-             Sign In
-         </div>
-         <div className="account">
-             Don't have an account? <Link to="/signup">Sign up</Link>
-         </div>
-     </div>
-     <div className="inputs-wrapper ">
-     <Form  render={formRenderProps => <FormElement style={{
-maxWidth: 650
-}}>
-     <fieldset className={'k-form-fieldset'}>         
-       <div className="mb-3">
-         <Field name={"email"} type={"email"} component={Input} label={"Email"} />
-       </div>
-       <div className="mb-3" style={{display: 'flex'}}>
-         <Field name={"password"} type={paswordType} component={Input} label={"Password"} />
-         <span className="k-icon k-i-preview password-icon" onClick={handleClick}></span>
-       </div>
-     </fieldset>
-     <fieldset className={'k-form-fieldset'}>         
-
-     <div className="mb-3">
-     <Checkbox label={'Remember Me'} />
-       </div>
-       </fieldset>
-
-     <div className="k-form-buttons">
-       <Link to="/home/dashboard" className="dashboard-button" style={{ textDecoration: 'none' }}>
-       <Button type={'submit'} className="sign-button">
-         Sign In
-        </Button>
-        </Link>
-     </div>
-   </FormElement>} />
-         
-     </div>
-     <div className="continue-with-wrapper">
-         <hr /> <span>Or continue with</span><hr/>
-     </div>
-     <div className="social-wrapper">
-         <a href="/#" className='facebook'>
-             <img src={require('../assets/facebook.png')} alt={'facebook icon'}></img>
-         </a>
-         <a href="/#" className='twitter'>
-             <img src={require('../assets/twitter.png')} alt={'twitter icon'}></img>
-         </a>
-         <a href="/#" className='reddit'>
-             <img src={require('../assets/reddit.png')} alt={'reddit icon'}></img>
-         </a>
-     </div>
- </div>
- </div>
-</div>
-
-
- 
-   )
-}
+                  <div className="k-form-buttons">
+                    <Link
+                      to="/home/dashboard"
+                      className="dashboard-button"
+                      style={{ textDecoration: "none" }}
+                    >
+                      <button type={"submit"} className="sign-button">
+                        Sign In
+                      </button>
+                    </Link>
+                  </div>
+                </FormElement>
+              )}
+            />
+          </div>
+        </div>
+        <div className="frame-wrapper"></div>
+      </div>
+    </div>
+  );
+};
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,78 +1,113 @@
-
-import { Form, Field, FormElement } from '@progress/kendo-react-form';
-import { Input } from '@progress/kendo-react-inputs';
-import { Checkbox } from '@progress/kendo-react-inputs';
-import { Link } from 'react-router-dom';
-import { Button } from '@progress/kendo-react-buttons';
+import { Form, Field, FormElement } from "@progress/kendo-react-form";
+import { Input } from "@progress/kendo-react-inputs";
+import { Checkbox } from "@progress/kendo-react-inputs";
+import { Link } from "react-router-dom";
 
 export const SignUp = () => {
-   return (
-      <div className="App">
+  return (
+    <div className="App">
       <div className="sign-in-page">
-      <div className="sign-in-wrapper">
-     <div className="logo-wrapper">
-         <div className="logo">
-         <img src={require('../assets/logo/Original on Transparent.png')} alt={'logo'} />
-         </div>
-         <div className='banner'>
-             Sign Up
-         </div>
-         <div className="account">
-         Already have an account?  <Link style={{ textDecoration: 'none' }} to="/">Sign In</Link>
-         </div>
-     </div>
-     <div className="inputs-wrapper">
-     <Form  render= {formRenderProps => <FormElement style={{ maxWidth: 650
-}}>
-   <fieldset className={'k-form-fieldset'}> 
-   <div className="mb-3">
-              <Field
-                name={"fullName"}
-                component={Input}
-                label={"Full Name"}
+        <div className="sign-in-wrapper">
+          <div className="logo-wrapper">
+            <div className="logo">
+              <img
+                src={require("../assets/logo/logo-white.png")}
+                alt={"sign in icon"}
               />
-            </div>        
-       <div className="mb-3">
-         <Field name={"email"} type={"email"} component={Input} label={"Email"} />
-       </div>
-       <div className="mb-3">
-         <Field name={"password"} type={"password"} component={Input} label={"Password"} />
-         <div className="mb-3">
-              <Field
-                name={"Company"}
-                component={Input}
-                label={"Company"}
-              />
-            </div> 
-       </div>
-       
-     </fieldset>
-   
-     <fieldset className={'k-form-fieldset'}>         
-     <div className="mb-3" style={{display: 'flex'}}>
-     <Checkbox />
-     <p> &nbsp; I agree to the <span style={{color: 'blue', textDecoration: 'underline'}}>Terms of Services </span> and <br></br>  <span style={{color: 'blue', textDecoration: 'underline'}}>Privacy Policy</span> </p>
-       </div>
-       <div className="k-form-buttons">
-       <Link to="/home/dashboard" className="dashboard-button">
-       <Button type={'submit'} className="sign-button">
-         Create Free Account 
-        </Button>
-        </Link>
-     </div>
-       </fieldset>
+            </div>
+            <div className="banner">Sign up</div>
+            <div className="account">
+              Already have an account?{" "}
+              <Link style={{ textDecoration: "none" }} to="/">
+                Sign In
+              </Link>
+            </div>
+          </div>
+          <div className="inputs-wrapper">
+            <Form
+              render={(formRenderProps) => (
+                <FormElement
+                  style={{
+                    maxWidth: 650,
+                  }}
+                >
+                  <fieldset className={"k-form-fieldset"}>
+                    <div className="mb-3">
+                      <Field
+                        name={"fullName"}
+                        component={Input}
+                        label={"Full Name"}
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <Field
+                        name={"email"}
+                        type={"email"}
+                        component={Input}
+                        label={"Email"}
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <Field
+                        name={"password"}
+                        type={"password"}
+                        component={Input}
+                        label={"Password"}
+                      />
+                      <div className="mb-3">
+                        <Field
+                          name={"Company"}
+                          component={Input}
+                          label={"Company"}
+                        />
+                      </div>
+                    </div>
+                  </fieldset>
 
+                  <fieldset className={"k-form-fieldset"}>
+                    <div
+                      className="mb-3 text-white"
+                      style={{ display: "flex" }}
+                    >
+                      <Checkbox />
+                      <p>
+                        {" "}
+                        &nbsp; I agree to the{" "}
+                        <span
+                          style={{
+                            color: "white",
+                            textDecoration: "underline",
+                          }}
+                        >
+                          Terms of Services{" "}
+                        </span>{" "}
+                        and <br></br>{" "}
+                        <span
+                          style={{
+                            color: "white",
+                            textDecoration: "underline",
+                          }}
+                        >
+                          Privacy Policy
+                        </span>{" "}
+                      </p>
+                    </div>
+                    <div className="k-form-buttons">
+                      <Link to="/home/dashboard" className="dashboard-button">
+                        <button type={"submit"} className="sign-button">
+                          Create Free Account
+                        </button>
+                      </Link>
+                    </div>
+                  </fieldset>
+                </FormElement>
+              )}
+            />
+          </div>
+        </div>
 
-   </FormElement>} />
-         
-     </div>
-
- </div>
- 
-
-
-</div>
-
- </div>
-   )
-}
+        <div className="frame-wrapper"></div>
+      </div>
+    </div>
+  );
+};
